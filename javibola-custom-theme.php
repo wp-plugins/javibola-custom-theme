5b8734586918933848425fb6d134d397362b5f83
JaviBola <JaviBola@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 11:28:52 +0000

    Activate custom theme
    git-svn-id: https://plugins.svn.wordpress.org/javibola-custom-theme/trunk@1134438 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- javibola-custom-theme.php
+++ javibola-custom-theme.php
@@ -8,55 +8,56 @@ Author: JaviBola.com
 Author URI: http://javibola.com
 License: GPL2
 */
-
 function javibola_custom_theme_install(){
 	//Actualizamos la variable en la instalación al tema actual.
-	add_option( 'jbct_theme', get_current_theme());
-	update_option( 'jbct_theme', get_current_theme());
+	$theme = wp_get_theme( $stylesheet, $theme_root );
+	$theme = $theme->Template;
+	add_option( 'jbct_theme', $theme);
+	update_option( 'jbct_theme', $theme);
 }
 register_activation_hook(__FILE__,'javibola_custom_theme_install');
 
 
-add_filter('template', 'jbct');
-add_filter('option_template', 'jbct');
-add_filter('option_stylesheet', 'jbct');
+if(get_option("jbct_theme")!= "" && get_option("jbct_theme") != "no-theme"){
+	add_filter('template', 'jbct');
+	add_filter('option_template', 'jbct');
+	add_filter('option_stylesheet', 'jbct');
+}
 function jbct($theme) {
 	if ( current_user_can('manage_options') ) {
 		$theme = get_option("jbct_theme");
 	}
-	return $theme;
+	if($theme != "" && $theme != "no-theme"){
+		return $theme;
+	}
 }
-	
 add_action( 'admin_menu', 'jbct_menu' );
-
 function jbct_menu() {
-	add_options_page( 'JaviBola Custom Theme', 'JaviBola Custom Theme', 'manage_options', 'jbct', 'jbct_options' );
+	add_options_page( 'JaviBola Custom Theme Test', 'JaviBola Custom Theme Test', 'manage_options', 'jbct', 'jbct_options' );
 }
-
 if(isset($_GET["jbct_theme"])){
 	update_option( 'jbct_theme', $_GET["jbct_theme"]);
 }
-
 function jbct_options() {
 	if ( !current_user_can( 'manage_options' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
-
 	$result = '<div class="wrap">';
 	$result .= '<h2>JaviBola Custom Theme Options</h2>';
 	if(isset($_GET["jbct_theme"])){
 		$result .= '<div class="updated" style="padding:20px;">Tema actualizado correctamente!</div>';
 	}
-	$result .= '<div class="">Tema actual para el admin: <b>'.get_option("jbct_theme").'</b></div>';
-
+	$result .= '<div class="">Tema actual para el admin: <b>';
+	if(get_option("jbct_theme")!= "" && get_option("jbct_theme") != "no-theme"){
+	$result.=get_option("jbct_theme");
+	}else{
+		$result .= get_current_theme();
+	}
+	$result.='</b></div>';
 	$directorio = get_template_directory();
-	
 	$directorio = explode("/", $directorio);
-	
 	array_pop($directorio);
 	$directorio = implode("/", $directorio);
-	
-	
 	if ($gestor = opendir($directorio)) {
 		//$result .= "Gestor de directorio: $directorio<br/>";
 		$result .= "<form action='options-general.php?page=jbct' method='GET'>";
@@ -64,7 +65,7 @@ function jbct_options() {
 		$result .= "<p>Selecciona uno de los temas para que sea visualizado cuando el admin esté registrado.</p>";
 		$result .= "<div>";
 		$result .= "<select name='jbct_theme'>";
-		$result .= "<option value='no-theme'>Sin tema</option>";
+		$result .= "<option value=''>Default</option>";
 		while (false !== ($entrada = readdir($gestor))) {
 			if(is_dir($directorio."/".$entrada) && $entrada != "." && $entrada != ".."){
 				$result .= "<option ".($entrada == get_option("jbct_theme") ? "selected='selected'" : "")." value='$entrada'>$entrada</option>";
@@ -78,7 +79,6 @@ function jbct_options() {
 	$result .= '<input type="hidden" name="page" value="jbct">';
 	$result .= '</form>';
 	$result .= '</div>';
-	
 	echo $result;
 }
 ?>
