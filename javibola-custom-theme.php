0d331d4c2138104894cba6c8596e278fb0a77822
JaviBola <JaviBola@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 14:52:47 +0000

    wp_get_themes included!
    git-svn-id: https://plugins.svn.wordpress.org/javibola-custom-theme/trunk@1136243 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- javibola-custom-theme.php
+++ javibola-custom-theme.php
@@ -3,27 +3,29 @@
 Plugin Name: JaviBola Custom Theme Test
 Plugin URI: http://javibola.com/javibola-custom-theme.zip
 Description: Enabled a Custom Theme if admin is logged for a safely testing.
-Version: 1.5
+Version: 1.6
 Author: JaviBola.com
 Author URI: http://javibola.com
 License: GPL2
 */
 function javibola_custom_theme_install(){
-	$theme_act = wp_get_theme(  );
-	$theme_act = $theme_act->Template;
-	add_option( 'jbct_theme', $theme_act);
-	update_option( 'jbct_theme', $theme_act);
+	//Actualizamos la variable en la instalación al tema actual.
+	$theme = wp_get_theme( $stylesheet, $theme_root );
+	$theme = $theme->Template;
+	add_option( 'jbct_theme', $theme);
+	update_option( 'jbct_theme', $theme);
 }
 register_activation_hook(__FILE__,'javibola_custom_theme_install');
-if(get_option("jbct_theme")!= "" && get_option("jbct_theme") != "no-theme" ){
-	if ( !has_filter( 'template', 'jbct' ) ){
-		add_filter('template', 'jbct', 11);
-	}
+
+if(get_option("jbct_theme")!= "" && get_option("jbct_theme") != "no-theme"){
+	add_filter('template', 'jbct');
 	add_filter('option_template', 'jbct');
 	add_filter('option_stylesheet', 'jbct');
 }
 function jbct($theme) {
-	$theme = get_option("jbct_theme");
+	if ( current_user_can('manage_options') ) {
+		$theme = get_option("jbct_theme");
+	}
 	if($theme != "" && $theme != "no-theme"){
 		return $theme;
 	}
@@ -40,7 +42,7 @@ function jbct_options() {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	$result = '<div class="wrap">';
-	$result .= '<h2>JaviBola Custom Theme Options</h2>';
+	$result .= '<h2>JaviBola Custom Theme Test Options</h2>';
 	if(isset($_GET["jbct_theme"])){
 		$result .= '<div class="updated" style="padding:20px;">Theme updated!</div>';
 	}
@@ -51,25 +53,19 @@ function jbct_options() {
 		$result .= get_current_theme();
 	}
 	$result.='</b></div>';
-	$directorio = get_template_directory();
-	$directorio = explode("/", $directorio);
-	array_pop($directorio);
-	$directorio = implode("/", $directorio);
-	if ($gestor = opendir($directorio)) {
-		$result .= "<form action='options-general.php?page=jbct' method='GET'>";
-		$result .= "<h3 class='title'>Select a theme</h3>";
-		$result .= "<p>Select a theme for display when admin is logged.</p>";
-		$result .= "<div>";
-		$result .= "<select name='jbct_theme'>";
-		$result .= "<option value=''>Default</option>";
-		while (false !== ($entrada = readdir($gestor))) {
-			if(is_dir($directorio."/".$entrada) && $entrada != "." && $entrada != ".."){
-				$result .= "<option ".($entrada == get_option("jbct_theme") ? "selected='selected'" : "")." value='$entrada'>$entrada</option>";
-			}
-		}
-		$result .= "</select>";
-		$result .= "</div>";
+	$themes = wp_get_themes();
+	$result .= "<form action='options-general.php?page=jbct' method='GET'>";
+	$result .= "<h3 class='title'>Select a theme</h3>";
+	$result .= "<p>Select a theme for display when admin is logged.</p>";
+	$result .= "<div>";
+	$result .= "<select name='jbct_theme'>";
+	foreach ($themes as $th){
+		$template = $th->Template;
+		$name = $th->Name;
+		$result .= "<option ".($template == get_option("jbct_theme") ? "selected='selected'" : "")." value='$template'>$name</option>";
 	}
+	$result .= "</select>";
+	$result .= "</div>";
 	$result .= '<br/>';
 	$result .= '<input type="submit" name="submit" id="submit" class="button button-primary" value="Save">';
 	$result .= '<input type="hidden" name="page" value="jbct">';
